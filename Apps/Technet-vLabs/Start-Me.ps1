3d3c13d595ed371c5a1e38cee11262d88362ebd4
Elliot Huffman <thelich2@gmail.com>
2018-07-26 08:03:12 -0400

    Update URLs

--- Apps/Technet-vLabs/Start-Me.ps1
+++ Apps/Technet-vLabs/Start-Me.ps1
@@ -1,6 +1,6 @@
 # Gitlab file URLs
-$InitialScript = "https://raw.githubusercontent.com/elliot-labs/Technet-vLab-Auto-Configure/master/Set-LabEnv.ps1"
-$PostRestartScript = "https://raw.githubusercontent.com/elliot-labs/Technet-vLab-Auto-Configure/master/Set-PostReboot.ps1"
+$InitialScript = "https://raw.githubusercontent.com/elliot-labs/Powershell-Doodads/master/Apps/Technet-vLabs/Set-LabEnv.ps1"
+$PostRestartScript = "https://raw.githubusercontent.com/elliot-labs/Powershell-Doodads/master/Apps/Technet-vLabs/Set-PostReboot.ps1"
 
 # Download the script files.
 Invoke-WebRequest -Uri $InitialScript -OutFile $env:TEMP\Set-LabEnv.ps1
