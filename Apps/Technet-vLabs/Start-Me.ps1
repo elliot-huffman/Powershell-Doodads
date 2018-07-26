6389e69b82ed5a4ab28643646640daeacc0f63e9
Elliot Huffman <thelich2@gmail.com>
2018-07-26 07:21:20 -0400

    import
    https://github.com/elliot-labs/Technet-vLab-Auto-Configure/blob/master/Start-Me.ps1

---
+++ Apps/Technet-vLabs/Start-Me.ps1
@@ -0,0 +1,16 @@
+# Gitlab file URLs
+$InitialScript = "https://raw.githubusercontent.com/elliot-labs/Technet-vLab-Auto-Configure/master/Set-LabEnv.ps1"
+$PostRestartScript = "https://raw.githubusercontent.com/elliot-labs/Technet-vLab-Auto-Configure/master/Set-PostReboot.ps1"
+
+# Download the script files.
+Invoke-WebRequest -Uri $InitialScript -OutFile $env:TEMP\Set-LabEnv.ps1
+Invoke-WebRequest -Uri $PostRestartScript -OutFile $env:TEMP\Set-PostReboot.ps1
+
+# Set the location for script execution
+Set-Location $env:TEMP
+
+# Start the initial Script.
+.\Set-LabEnv.ps1
+
+# Prep post restart script to run after reboot.
+    # Placeholder
