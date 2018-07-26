ff232b39fb85720989df28f3a6cbdd01ed2d06f3
Elliot Huffman <thelich2@gmail.com>
2018-07-26 05:46:40 -0400

    Finalize script
    Finish the conversion from the get-computerdomain script to the get-userdomain.
    Script wording has been updated and now searches for users properly.

--- Libraries/AD DS/Get-UserDomain.ps1
+++ Libraries/AD DS/Get-UserDomain.ps1
@@ -1,17 +1,17 @@
 <#
 .SYNOPSIS
-This script provides the domain of the specified computer.
+This script provides the domain of the specified user.
 .DESCRIPTION
-This script looks up a computer's domain based upon the name of the computer.
+This script looks up a user's domain based upon the name of the user.
 .PARAMETER UserName
-If specified, a search for the specified name will be executed. Default: Use the local computer's name for the search.
+If specified, a search for the specified name will be executed. Default: Use the local user's name for the search.
 .EXAMPLE
 Get-UserDomain
 This will gather the local user's NT Alias and will run a search on the domain for the users's domain.
 If found then it will output the name of the domain that the user is currently located in.
 .EXAMPLE
-Get-UserDomain -UserName "other-computer"
-This will search all domains in the forrest and will return the domain of the specified computer account.
+Get-UserDomain -UserName "elliot"
+This will search all domains in the forrest and will return the domain of the specified user account.
 .NOTES
 RSAT's Active directory powershell needs to be installed and enabled for this script to work.
 Domain controller connectivity needs to be present for the Active directory searches to be successful.
@@ -29,14 +29,14 @@ $Domains = (Get-ADForest).Domains
 foreach ($Domain in $Domains) {
     # Use a try catch block to silence errors.
     try {
-        # Record the results of the computer lookup for the curent domain.
-        $ComputerData = Get-ADUser -Identity $ComputerName -Server $Domain | Select-Object DistinguishedName
+        # Record the results of the user lookup for the curent domain.
+        $UserData = Get-ADUser -Identity $UserName -Server $Domain | Select-Object DistinguishedName
     } catch {} # Do nothing
 }
 
 # Split each section of the distinguishd name into seperate entitities.
-$ComputerDomain = $ComputerData -split "," | Where-Object {$_ -like "DC=*"}
+$UserDomain = $UserData -split "," | Where-Object {$_ -like "DC=*"}
 # Join them back together and remove the trailing close currly bracket.
-$ComputerDomain = ($ComputerDomain -join "." -replace ("DC=", "")).TrimEnd("}")
+$UserDomain = ($UserDomain -join "." -replace ("DC=", "")).TrimEnd("}")
 # Make the output available to other scripts.
-Write-Output $ComputerDomain
+Write-Output $UserDomain
