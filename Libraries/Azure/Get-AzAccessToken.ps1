824c38c9c52be082c56da932a61790744bcdebcf
Elliot Huffman <thelich2@gmail.com>
2019-05-30 22:32:09 -0500

    Require module
    Requires the Az.Accounts module to run.

--- Libraries/Azure/Get-AzAccessToken.ps1
+++ Libraries/Azure/Get-AzAccessToken.ps1
@@ -1,3 +1,5 @@
+#Requires -Modules Az.Accounts
+
 $Account = Connect-AzAccount
 $TenantID = (Get-AzSubscription -SubscriptionId "e5b8499c-217c-4eef-b3e8-4942085b6a51").TenantId
 $Tokens = $account.Context.TokenCache.ReadItems()
