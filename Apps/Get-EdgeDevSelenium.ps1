d5864cdd3d095de98f17c88c7b88c55c47d1d79f
Elliot Huffman <ehuffman@elliot-labs.com>
2019-10-19 13:27:36 -0400

    Quick refactor
    Refactored the code to follow better practices.
    Hard coded values now are automatically determined or are available as a parameter.
    Added more comments.
    Added parameters.
    Added Cmdlett binding.
    Properly quote parameters that are strings.
    Formatting.

--- Apps/Get-EdgeDevSelenium.ps1
+++ Apps/Get-EdgeDevSelenium.ps1
@@ -1,30 +1,40 @@
 #Requires -RunAsAdministrator
 
-# Check to see if microsoft edge is installed
-if (!(Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe")) {
-    Write-Error -Message "Microsoft Edge Dev needs to be installed to run this script!"
-    exit
+[CmdletBinding(SupportsShouldProcess=$true)]
+[OutputType([Void])]
+param(
+    [System.String]$RegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe",
+    [System.String]$SeleniumDirectory = "C:\EdgeSelenium"
+)
+
+# Check to see if Microsoft Edge is installed
+if (-not (Test-Path -Path $RegistryPath)) {
+    # Write an error if Edge was not found
+    Write-Error -Message "Microsoft Edge Chrome needs to be installed to run this script!"
+    
+    # Exit the script and set a return code
+    exit 1
 }
 
-# Currently installed version of MS Edge Dev
-$EdgeVersion = (Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe').'(Default)').VersionInfo.ProductVersion
+# Get the currently installed version of MS Edge Chrome
+$EdgeVersion = (Get-Item (Get-ItemProperty $RegistryPath).'(Default)').VersionInfo.ProductVersion
 
-# Check the CPU to see if ti si supported
-if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
-    $CPUArch = 64
-} elseif ($env:PROCESSOR_ARCHITECTURE -eq "x86") {
-    $CPUArch = 32
-} else {
-    Write-Error -Message "CPU not supported"
-    exit
+# Check the CPU architecture to see if it is supported
+switch ($env:PROCESSOR_ARCHITECTURE) {
+    "AMD64" { $CPUArch = 64; break }
+    "x86" { $CPUArch = 32; break }
+    Default {
+        Write-Error -Message "CPU not supported"
+        exit 2
+    }
 }
 
-# Grab the edge dev web driver executable
-if (!(Test-Path -Path "C:\Edge")) {
-    New-Item -Path "C:\Edge" -ItemType Directory
+# Grab the edge chrome web driver executable
+if ( -not (Test-Path -Path $SeleniumDirectory)) {
+    New-Item -Path $SeleniumDirectory -ItemType "Directory"
 
-    # Execute commands relative to the edge folder (should already exist with the edge web driver renamed to chromedriver.exe)
-    Set-Location -Path C:\Edge
+    # Execute commands relative to the Edge folder that was just created
+    Set-Location -Path $SeleniumDirectory
 
     # Download the correct version of the web driver based upon the currently installed version of EdgeDev
     # Download the latest version of NuGet
@@ -33,45 +43,45 @@ if (!(Test-Path -Path "C:\Edge")) {
     Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile .\NuGet.exe
     $ProgressPreference = "Continue"
 
-    # Load teh file compression lib
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    # Load the file compression lib
+    Add-Type -AssemblyName "System.IO.Compression.FileSystem"
 
-    $CurrentDir = Resolve-Path -Path .\
+    $CurrentDir = Resolve-Path -Path ".\"
     # Extract the downloaded zip file to the current directory
     [System.IO.Compression.ZipFile]::ExtractToDirectory("$CurrentDir\WebDriver.zip", "$CurrentDir\")
 
     # Clean up after extraction
-    Remove-Item -Path .\WebDriver.zip
+    Remove-Item -Path ".\WebDriver.zip"
 
     # Rename the driver to the one selenium is expecting
-    Rename-Item -Path .\msedgedriver.exe -NewName chromedriver.exe
+    Rename-Item -Path ".\msedgedriver.exe" -NewName "chromedriver.exe"
 
     # Install selenium lib
-    .\NuGet.exe install Selenium.WebDriver
+    .\NuGet.exe --% install Selenium.WebDriver
 
 } else {
     # Execute commands relative to the edge folder (should already exist with the edge web driver renamed to chromedriver.exe)
-    Set-Location -Path C:\Edge
+    Set-Location -Path $SeleniumDirectory
 }
 # Web driver needs to be on the system path
-$env:Path += ";C:\Edge\"
+$env:Path += ";$SeleniumDirectory"
 
 # Edge binary needs to be on the system path
-$env:Path += ";C:\Program Files (x86)\Microsoft\Edge Dev\Application"
+$env:Path += ";$(((Get-ItemProperty $RegistryPath).'(Default)' | Split-Path -Parent) + `"\`")"
 
 # import the selenium lib
-Add-Type -Path .\Selenium.WebDriver.3.141.0\lib\net45\WebDriver.dll
+Add-Type -Path ".\Selenium.WebDriver.3.141.0\lib\net45\WebDriver.dll"
 
 # Instantiate a web driver instance
 $Driver = [OpenQA.Selenium.Chrome.ChromeDriver]::new()
 
 # Navigate to awesomeness
-$Driver.Navigate().GoToUrl("https://github.com/elliot-labs")
+$Driver.Navigate().GoToUrl("https://github.com/elliot-labs/PowerShell-Doodads")
 
 # Prevent instant browser close after navigation
 Read-Host -Prompt "Press enter to continue" | Out-Null
 
-# close the web browser
+# Close the web browser
 $Driver.Close()
 
 # Close the driver session to allow for a clean exit instead of a hang on script completion
