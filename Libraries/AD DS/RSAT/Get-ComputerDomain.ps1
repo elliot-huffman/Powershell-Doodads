d8ad0a7961abf30bd1c410448ac578e87d02bbbb
Elliot Huffman <ehuffman@elliot-labs.com>
2019-11-10 20:51:12 -0500

    Add Debug Support
    Add the ability to return debug information if the user drops the script into debug mode.

--- Libraries/AD DS/RSAT/Get-ComputerDomain.ps1
+++ Libraries/AD DS/RSAT/Get-ComputerDomain.ps1
@@ -60,13 +60,29 @@ Param(
 
 # Allow the script to be run in a pipeline
 process {
+    # Write debugging info
+    Write-Debug -Message "$(Get-Date -Format "HH:mm:ss") - Parameter info:"
+    Write-Debug -Message "$(Get-Date -Format "HH:mm:ss") - `$ComputerName: $ComputerName"
+    Write-Debug -Message "$(Get-Date -Format "HH:mm:ss") - `$SearchDomain: $SearchDomain"
+
     # Create an empty array for multiple potential matches
     $ComputerDN = @()
 
     # Iterate through each domain
     foreach ($Domain in $SearchDomain) {
+        # Write verbose info
+        Write-Verbose -Message "Running domain controller query for computer: $ComputerName"
+        
+        # Write debug info
+        Write-Debug -Message "$(Get-Date -Format "HH:mm:ss") - Executing query against $Domain for $ComputerName"
+
+        # Query the specified domain for the specified computer and extract the distinguished name
         $ComputerDN += (Get-ADComputer -Identity $ComputerName -Server $Domain -ErrorAction "SilentlyContinue").DistinguishedName
     }
+
+    # Write debug info
+    Write-Debug -Message "$(Get-Date -Format "HH:mm:ss") - List of DNs for $ComputerName`:"
+    $ComputerDN | ForEach-Object -Process {Write-Debug -Message "$_"}
 
     # If no computer is found with the above query, exit the script
     if ($ComputerDN.Count -eq 0) {
@@ -82,9 +98,15 @@ process {
         # Split each section of the distinguished name into separate entities
         $SplitDN = $DN -Split "," | Where-Object -FilterScript { $_ -like "DC=*" }
 
+        # Write debug info
+        Write-Debug -Message "$(Get-Date -Format "HH:mm:ss") - List of split DNs for $DN`:"
+        $SplitDN | ForEach-Object -Process {Write-Debug -Message "$_"}
 
         # Join them back together and remove the trailing close currly bracket
         $FQDN = ($SplitDN -join ".") -Replace @("DC=", "")
+
+        # Write debug info
+        Write-Debug -Message "$(Get-Date -Format "HH:mm:ss") - FQDN: $FQDN"
 
         # Make the output available to other scripts
         $PSCmdlet.WriteObject($FQDN)
