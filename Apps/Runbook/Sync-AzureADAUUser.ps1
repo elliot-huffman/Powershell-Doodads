09df39205e2cda242abfa681fb17a77d9212641c
Elliot Huffman <ehuffman@elliot-labs.com>
2021-05-11 20:41:28 -0400

    Rename Parameters
    Rename the matching parameters so that they are easier to understand. While they technically are blob matches, most people don't know them as blob matches.
    Updated the docs to match, lol get it, "to match" :-P

--- Apps/Runbook/Sync-AzureADAUUser.ps1
+++ Apps/Runbook/Sync-AzureADAUUser.ps1
@@ -7,24 +7,24 @@
 .PARAMETER AdminUnitID
     This parameter is the GUID/ObjectID of the Administrative Unit that needs to have the users synced to.
     Example Value: 8f14a65f-3032-42c8-a196-1cf66d11b930
-.PARAMETER UPNBlobMatchString
-    This parameter is used to specify the positive match of the UPN.
+.PARAMETER UPNMatch
+    This parameter is used to specify the positive match of the UPN using blob matching (powershell wildcard matching e.g. -like).
     A positive match will add a user to the list of users to be synced.
     A negative match will override a positive match.
     E.g. "*@elliot-labs.com" will match any UPN that has @elliot-labs.com at the end of it, for example ehuffman@elliot-labs.com will match.
-.PARAMETER UPNNegativeBlobMatch
-    This parameter is used to specify the negative match of the UPN.
+.PARAMETER UPNNegativeMatch
+    This parameter is used to specify the negative match of the UPN using blob matching (powershell wildcard matching e.g. -NotLike).
     A negative match will override a positive match.
     A negative match will exclude the user from the sync if the match is successful.
     E.G. "priv-*" will exclude all UPNs that have a prefix of priv-. so an email like this will be excluded form the AU sync: "priv-ehuffman@elliot-labs.com".
 .PARAMETER ExcludedUserGUID
     This parameter is used to explicitly exclude users.
     A match of the user's GUID with the value provided will exclude the user from sync.
-    Use commas to separate GUIDs to exclude more than one object. Do not put a space after the comma.
+    Use commas to separate GUIDs to exclude more than one object.
     Example value: "8f14a65f-3032-42c8-a196-1cf66d11b930"
     Example values: 8f14a65f-3032-42c8-a196-1cf66d11b930,00000000-0000-0000-0000-000000000000"
 .EXAMPLE
-    PS C:\> Sync-AzureADAUUser.ps1 -AdminUnitID "00000000-0000-0000-0000-000000000000" -UPNBlobMatchString "*@example.com" -UPNNegativeBlobMatch "*priv*"
+    PS C:\> Sync-AzureADAUUser.ps1 -AdminUnitID "00000000-0000-0000-0000-000000000000" -UPNMatch "*@example.com" -UPNNegativeMatch "*priv*"
     Reads all of the users in the AAD and syncs them into the specified Administrative Unit (00000000-0000-0000-0000-000000000000).
     If the user has a UPN that ends in "@example.com" it will be included in the list.
     If the user has "priv" anywhere in its name then it will be excluded.
@@ -45,9 +45,9 @@ param(
     [GUID]$AdminUnitID,
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
-    [String]$UPNBlobMatchString = "*@example.com",
+    [String]$UPNMatch = "*@example.com",
     [ValidateNotNullOrEmpty()]
-    [String]$UPNNegativeBlobMatch,
+    [String]$UPNNegativeMatch,
     [ValidateNotNullOrEmpty()]
     [String]$ExcludedUserGUID
 )
@@ -116,7 +116,7 @@ do {
 # Loop through each user in the list to ensure they are in the correct AU
 foreach ($User in $UserList) {
     # Execute the blob matches on the UPN to ensure it is the correct UPN format.
-    if (($User.UserPrincipalName -like $UPNBlobMatchString) -and ($User.UserPrincipalName -NotLike $UPNNegativeBlobMatch)) {
+    if (($User.UserPrincipalName -like $UPNMatch) -and ($User.UserPrincipalName -NotLike $UPNNegativeMatch)) {
         # Only add users that are not in the AU, as if the user is already in it, it will throw an error.
         if (($User.Id -NotIn $AUUserGuidList) -and ($User.ID -NotIn $ExcludedUserGUID)) {
             # Expose the current user's object ID
