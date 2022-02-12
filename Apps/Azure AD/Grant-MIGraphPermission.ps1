d3eef9a15fd8906956022e9590b89522f722d6fb
Elliot Huffman <ehuffman@elliot-labs.com>
2022-02-12 10:51:25 -0600

    wording

--- Apps/Azure AD/Grant-MIGraphPermission.ps1
+++ Apps/Azure AD/Grant-MIGraphPermission.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Grants Graph API Permissions to MAnaged Identity Service Principals
+    Grants Graph API Permissions to Managed Identity Service Principals
 .DESCRIPTION
     App registration is not supported as it has a really good GUI in the Azure AD Portal.
     Returns false on failure
@@ -36,9 +36,6 @@
 .NOTES
     This script requires the Microsoft.Graph.Authentication and Microsoft.Graph.Applications modules to be installed before execution.
     If the GUI mode is to be used, ISE must be installed as some Windows Forms components are included in ISE that this script uses (Out-GridView).
-    
-    Exit Codes:
-        1 - lorem ipsum
 #>
 
 # Ensure the appropriate pre-requirements for the script
